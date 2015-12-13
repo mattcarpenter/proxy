dd724444fd6cfadfaee2355bd152d906979707ef
Matt Carpenter <mattcarpenter@gmail.com>
2015-12-13 21:22:21 +0000

    server now responds with proper keys

--- server.js
+++ server.js
@@ -3,21 +3,35 @@ var https = require('https');
 var httpProxy = require('http-proxy');
 var fs = require('fs');
 
-var proxy = httpProxy.createProxyServer({});
-
-var options = {
-	key: fs.readFileSync('keys/key.pem'),
-	cert: fs.readFileSync('keys/cert.pem')
+var read = require('fs').readFileSync;
+var privateKey = read('keys/key.pem', 'utf8');
+var certificate = read('keys/mattcarpenter_io.crt', 'utf8');
+var chainLines = read('keys/mattcarpenter_io.ca-bundle', 'utf8').split("\n");
+var cert = [];
+var ca = [];
+chainLines.forEach(function(line) {
+  cert.push(line);
+  if (line.match(/-END CERTIFICATE-/)) {
+    ca.push(cert.join("\n"));
+    cert = [];
+  }
+});
+var credentials = {
+  "key": privateKey,
+  "cert": certificate,
+  "ca": ca
 };
 
 var server = http.createServer(onConnection);
-var secureServer = https.createServer(options, onConnection);
+var secureServer = https.createServer(credentials, onConnection);
 
 server.listen(3000);
 secureServer.listen(3001);
 
 function onConnection(req, res) {
 	console.log(req.url);
+	res.end('foo!!!');
+return;
 	proxy.web(req, res, {
 		target: req.url
 	});
